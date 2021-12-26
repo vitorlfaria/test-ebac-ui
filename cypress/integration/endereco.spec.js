ced9df7fbaaf96ddb0ee0827b0ed111c2227a354
vitorlfaria <vitorlacerdafaria7@gmail.com>
2021-12-26 09:00:41 -0300

    Utilizando Page Objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade endereços - Faturamento e Entrega', () => {
     
@@ -10,7 +12,23 @@ describe('Funcionalidade endereços - Faturamento e Entrega', () => {
     })
     
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
-        cy.get(':nth-child(1) > .title > .edit').click()
+        EnderecoPage.editarEnderecoFaturamento('Vitor', 'Lacerda', 'Elite', 'Brasil', 'Av. Brasil', '3010', 'Curitiba', 'Paraná', '80630-420', '419999999999', 'vitor@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+    
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empresa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].endereco,
+            dadosEndereco[0].numero,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].cep,
+            dadosEndereco[0].telefone,
+            dadosEndereco[0].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
